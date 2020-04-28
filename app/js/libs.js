55d77df7a194f14ff4d995efc4f56f6b712671eb
PDM-pavlyukdm <pavlyukdm@gmail.com>
2020-04-28 20:42:49 +0300

    created path to js libs
    Created libs.js in app/js which includes paths to vendor folder with js libs files

---
+++ app/js/libs.js
@@ -0,0 +1,11 @@
+/*
+*Need restart gulp after editing list of scripts
+*/
+
+module.exports = function jslibs (){
+	return [
+		'./app/js/vendor/jquery.min.js',
+		'./app/js/vendor/swiper.min.js'
+	];
+}
+
