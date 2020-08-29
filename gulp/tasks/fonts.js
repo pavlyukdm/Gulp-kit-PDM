74533d3c21607e9f3cfef860c0d0af024ce1e2bc
PDM-pavlyukdm <pavlyukdm@gmail.com>
2020-08-29 18:09:45 +0300

    Add fonts converting

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp')
+const ttf2woff = require('gulp-ttf2woff')
+const ttf2woff2 = require('gulp-ttf2woff2')
+
 
 module.exports = 	gulp.task('fonts', function () {
-	return gulp.src('app/fonts/**/*')
+	gulp.src('app/fonts/**/*')
+		.pipe(ttf2woff())
+		.pipe(gulp.dest('dist/fonts/'))
+		return gulp.src('app/fonts/**/*')
+		.pipe(ttf2woff2())
 		.pipe(gulp.dest('dist/fonts/'))
 })
