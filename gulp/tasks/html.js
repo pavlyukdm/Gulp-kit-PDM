8697668d100b5ec3d4c12b61d729f6c8e9bf4a4f
PDM-pavlyukdm <pavlyukdm@gmail.com>
2020-08-30 11:57:56 +0300

    Add file include plugin
    Add gulp-file-include plugin, change structure html files (add folder _includes)

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp')
+const fileinclude = require('gulp-file-include');
 
 module.exports = 	gulp.task('html', function () {
-	return gulp.src('app/**/*.html')
-		.pipe(gulp.dest('dist/'))
+	return gulp.src('app/[^_]*.html')
+	.pipe(fileinclude())
+	.pipe(gulp.dest('dist/'));
+	// return 	gulp.src('app/[^_]*.html')
+	// .pipe(fileinclude({
+	// 	prefix: '@@',
+	// 	basepath: '@file'
+	// }))
+	// 	.pipe(gulp.dest('dist/'))
 })
