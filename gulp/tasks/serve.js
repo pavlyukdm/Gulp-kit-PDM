5b44a97d09a83904d0fa164539ae6f53c4cedb95
PDM-pavlyukdm <pavlyukdm@gmail.com>
2020-10-20 11:36:04 +0300

    Add img refresh online
    Add image watch task to serve task

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -7,12 +7,13 @@ module.exports = 	gulp.task('serve', function (cb) {
 			baseDir: "./dist"
 		}
 	})
-	// gulp.watch("app/view/**/*", gulp.series("html")).on('change', browserSync.reload);
 	gulp.watch("app/**/*.html", gulp.series("html")).on('change', browserSync.reload);
 	gulp.watch("app/styles/**/*", gulp.series("styles:main", cb => gulp.src('dist/css').pipe(browserSync.stream()).on('end', cb)));
 	gulp.watch("app/styles/**/*", gulp.series("styles:libs", cb => gulp.src('dist/css').pipe(browserSync.stream()).on('end', cb)));
 	gulp.watch("app/js/**/*", gulp.series("js:main")).on('change', browserSync.reload);
 	gulp.watch("app/js/**/*", gulp.series("js:libs")).on('change', browserSync.reload);
+	gulp.watch("app/img/**/*", 	gulp.series("imageMinifyTinyPNG")).on('change', browserSync.reload);
+	gulp.watch("app/img/**/*", 	gulp.series("imageMinifyImagemin")).on('change', browserSync.reload);
 
 	return cb
 })
