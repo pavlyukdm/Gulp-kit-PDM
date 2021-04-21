f43e8086add53475750e48dd91db14ed34c8d938
PDM-pavlyukdm <pavlyukdm@gmail.com>
2021-04-21 23:49:55 +0300

    Revert "Add gulp-useref, fix js min maps"
    This reverts commit c727eb792591d6ef3b96fe571c39a9278caab708.

--- app/js/libs.js
+++ app/js/libs.js
@@ -1,13 +1,12 @@
 /*
-*Gulp restart is needed after editing list of scripts
+*Need restart gulp after editing list of scripts
 */
 
 module.exports = function jslibs (){
 	return [
-		// './app/js/vendor/jquery.min.js',
-		// './app/js/vendor/slick.min.js',
-		// './node_modules/jquery/dist/jquery.js',
-		// './node_modules/slick-carousel/slick/slick.js',
+		'./app/js/vendor/jquery.min.js',
+		//'./app/js/vendor/swiper.min.js',
+		'./app/js/vendor/slick.min.js'
 	];
 }
 
--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,7 +1,22 @@
 const gulp = require('gulp')
+const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const rename = require('gulp-rename')
+const jslibspath = require('../../app/js/libs')
 const paths = require('../paths')
 
-module.exports = gulp.task('js', function () {
+module.exports = gulp.task('js:main', function () {
 	return gulp.src(paths.src.js)
+		.pipe(gulp.dest(paths.build.js))
+})
+
+module.exports = gulp.task('js:libs', function () {
+	return gulp.src(jslibspath())
+		.pipe(sourcemaps.init())
+		.pipe(concat('libs.js', {newLine: '\r\n'}))
+		.pipe(sourcemaps.write('/'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
 		.pipe(gulp.dest(paths.build.js))
 })
