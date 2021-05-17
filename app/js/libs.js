2ed118cd27f96a7daca865a330b8c15edd8cbe8b
PDM-pavlyukdm <pavlyukdm@gmail.com>
2021-05-17 18:35:37 +0300

    Change js libs path structure
    Change js libs path structure
    Now preferable way to include libs is including from node-modules folder

--- app/js/libs.js
+++ app/js/libs.js
@@ -1,11 +1,11 @@
 /*
 *Gulp restart is needed after list of scripts edit
-*Better to put in app/vendor folder unminified files because of better source mapping
+*Better to include unminified files because of better source mapping
 */
 
 module.exports = function jslibs (){
 	return [
-		'./app/js/vendor/jquery.js',
+		'./node_modules/jquery/dist/jquery.js',
 	];
 }
 
